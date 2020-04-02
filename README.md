7534b392a2cdc33d2c62bd0ab002d11f0a68d880
Jan Sprinz <neo@neothethird.de>
2020-04-02 16:31:46 +0200

    Deprecate devices api, replace deprecated request pacakge with axios, implement proper unit-tests

--- README.md
+++ README.md
@@ -4,75 +4,18 @@
 
 This is a nodejs module to talk to ubports apis and services, built using modern javascript.
 
-**WIP**: More APIs will be added in the future.
-
 ## Docs
 
 This package combines modules for different modules to access different APIs:
 
-- [Devices](#Devices): Access basic information about every device.
-- [Installer](#Installer): Access the Installer API.
+- Devices: (**DEPRECATED**) Access basic information about every device
+- Installer: Access the UBports Installer API
 
 ### Basic usage example
 
 ```javascript
 const UbpApi = require("./src/module.js");
 
-const devices = new UbpApi.Devices();
 const installer = new UbpApi.Installer();
-
-devices.getNotWorking("FP2").then((notWorking) => console.log("Not working: " + notWorking));
+installer.getDevices().then(console.log);
 ```
-
-### Devices
-
-The constructor takes an object with optional properties as an argument. The default properties are listed below.
-
-```javascript
-{
-  host: "https://devices.ubports.com/", // URL of the devices api
-  allow_insecure: false                 // allow unencrypted URL
-  cache_time: 180                       // time to keep cached files
-}
-```
-#### Primitive Calls
-
-##### getDevices()
-
-- [Device - Get all core devices](https://api.ubports.com/#api-Device-GetDevices)
-- [CommunityDevice - Get all community devices](https://api.ubports.com/#api-CommunityDevice-GetCommunityDevices)
-
-Returns a promise for an array of devices.
-
-```javascript
-devices.getDevices().then((devices) => for(device in devices) console.log(device.name));
-```
-
-##### getDevice(device)
-
-- [Device - Get core device](https://api.ubports.com/#api-https://api.ubports.com/#api-Device-GetDevice)
-- [CommunityDevice - Get community device](https://api.ubports.com/#api-CommunityDevice-GetCommunityDevice)
-
-Returns a promise for a device object.
-
-```javascript
-devices.getDevice("bacon").then((device) => console.log(device.info));
-```
-
-#### Convenience Functions
-
-##### getNotWorking(device)
-
-Returns a promise for a list of broken features, returns false if no bugs are known.
-
-```javascript
-devices.getNotWorking("FP2").then((notWorking) => {
-  if (notWorking)
-    console.log("Not working: " + device.info);
-  else {
-    console.log("Everything works").
-  }
-});
-```
-
-## Installer
--- tests/unit-tests/test_devices.js
+++ tests/unit-tests/test_devices.js
@@ -18,7 +18,6 @@
  */
 
 const fs = require("fs");
-const request = require("request");
 
 const chai = require("chai");
 var sinonChai = require("sinon-chai");
@@ -33,186 +32,13 @@ const deviceFP2Json = require("../test-data/device-FP2.json");
 
 describe("Devices module", function() {
   describe("constructor()", function() {
-    it("should create default devices-api-client", function() {
-      const api = new Devices();
-      expect(api.host).to.eql("https://api.ubports.com/v1/devices/");
-    });
-
-    it("should create custom devices-api-client", function() {
-      const api = new Devices({ host: "https://api.example.com/" });
-      expect(api.host).to.eql("https://api.example.com/");
-    });
-
-    it("should ensure trailing slash", function() {
-      const api = new Devices({ host: "https://api.example.com" });
-      expect(api.host).to.eql("https://api.example.com/");
-    });
-
-    it("should return insecure error", function() {
+    it("should throw deprecation warning", function(done) {
       try {
-        const api = new Devices({ host: "http://api.example.com/" });
-      } catch (err) {
-        expect(err.message).to.equal(
-          "Insecure URL! Call with allow_insecure to ignore."
-        );
+        const api = new Devices();
+        done("error: should have thrown");
+      } catch (e) {
+        done();
       }
-    });
-
-    it("should create insecure devices-api-client", function() {
-      const api = new Devices({
-        host: "http://api.example.com/",
-        allow_insecure: true
-      });
-      expect(api.host).to.eql("http://api.example.com/");
-    });
-
-    it("should return invalid url error", function() {
-      try {
-        const api = new Devices({ host: "definitely not a valid url" });
-      } catch (err) {
-        expect(err.message).to.equal("Host is not a valid URL!");
-      }
-    });
-  });
-
-  describe("getDevices()", function() {
-    it("should return devices", function() {
-      const requestStub = this.sandbox
-        .stub(request, "get")
-        .callsFake(function(url, cb) {
-          cb(false, { statusCode: 200 }, devicesJson);
-        });
-
-      const api = new Devices();
-      return api.getDevices().then(result => {
-        expect(result).to.eql(devicesJson);
-        expect(requestStub).to.have.been.calledWith({
-          url: "https://api.ubports.com/v1/devices/",
-          json: true,
-          timeout: 2000
-        });
-      });
-    });
-
-    it("should return error", function() {
-      const requestStub = this.sandbox
-        .stub(request, "get")
-        .callsFake(function(url, cb) {
-          cb(true, { statusCode: 500 }, devicesJson);
-        });
-
-      const api = new Devices();
-      return api
-        .getDevices()
-        .then(() => {})
-        .catch(err => {
-          expect(err).to.eql(true);
-          expect(requestStub).to.have.been.calledWith({
-            url: "https://api.ubports.com/v1/devices/",
-            json: true,
-            timeout: 2000
-          });
-        });
-    });
-  });
-
-  describe("getDevice()", function() {
-    it("should return devices", function() {
-      const requestStub = this.sandbox
-        .stub(request, "get")
-        .callsFake(function(url, cb) {
-          cb(false, { statusCode: 200 }, deviceBaconJson);
-        });
-
-      const api = new Devices();
-      return api.getDevice("bacon").then(result => {
-        expect(result).to.eql(deviceBaconJson);
-        expect(requestStub).to.have.been.calledWith({
-          url: "https://api.ubports.com/v1/devices/bacon",
-          json: true,
-          timeout: 2000
-        });
-      });
-    });
-
-    it("should return error", function() {
-      const requestStub = this.sandbox
-        .stub(request, "get")
-        .callsFake(function(url, cb) {
-          cb(true, { statusCode: 500 }, deviceBaconJson);
-        });
-
-      const api = new Devices();
-      return api
-        .getDevice("bacon")
-        .then(() => {})
-        .catch(err => {
-          expect(err).to.eql(true);
-          expect(requestStub).to.have.been.calledWith({
-            url: "https://api.ubports.com/v1/devices/bacon",
-            json: true,
-            timeout: 2000
-          });
-        });
-    });
-  });
-
-  describe("getNotWorking()", function() {
-    it("should return false", function() {
-      const requestStub = this.sandbox
-        .stub(request, "get")
-        .callsFake(function(url, cb) {
-          cb(false, { statusCode: 200 }, deviceBaconJson);
-        });
-
-      const api = new Devices();
-      return api.getNotWorking("bacon").then(result => {
-        expect(result).to.eql(false);
-        expect(requestStub).to.have.been.calledWith({
-          url: "https://api.ubports.com/v1/devices/bacon",
-          json: true,
-          timeout: 2000
-        });
-      });
-    });
-
-    it("should return what's not working", function() {
-      const requestStub = this.sandbox
-        .stub(request, "get")
-        .callsFake(function(url, cb) {
-          cb(false, { statusCode: 200 }, deviceFP2Json);
-        });
-
-      const api = new Devices();
-      return api.getNotWorking("FP2").then(result => {
-        expect(result).to.eql(["GPS"]);
-        expect(requestStub).to.have.been.calledWith({
-          url: "https://api.ubports.com/v1/devices/FP2",
-          json: true,
-          timeout: 2000
-        });
-      });
-    });
-
-    it("should return error", function() {
-      const requestStub = this.sandbox
-        .stub(request, "get")
-        .callsFake(function(url, cb) {
-          cb(true, { statusCode: 500 }, deviceBaconJson);
-        });
-
-      const api = new Devices();
-      return api
-        .getNotWorking("bacon")
-        .then(() => {})
-        .catch(err => {
-          expect(err).to.eql(true);
-          expect(requestStub).to.have.been.calledWith({
-            url: "https://api.ubports.com/v1/devices/bacon",
-            json: true,
-            timeout: 2000
-          });
-        });
     });
   });
 });
